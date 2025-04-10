c0d7125fb76faa3c933d043556a09b5b1dcd98aa
idrissalami01 <88428744+idrissalami01@users.noreply.github.com>
2025-04-10 17:57:41 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,41 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+                
+
+@app.route('/decrypt/<string:token>')
+def decryptage(token):
+    try:
+        token_bytes = token.encode()
+        decrypted = f.decrypt(token_bytes)
+        return f"Valeur décryptée : {decrypted.decode()}"
+    except Exception as e:
+        return f"Erreur lors du déchiffrement : {str(e)}"
+@app.route('/encrypt/<key>/<valeur>')
+def encrypt_personnalise(key, valeur):
+    try:
+        fernet = Fernet(key.encode())
+        valeur_bytes = valeur.encode()
+        token = fernet.encrypt(valeur_bytes)
+        return f"Valeur encryptée : {token.decode()}"
+    except Exception as e:
+        return f"Erreur lors du chiffrement : {str(e)}"
+
+@app.route('/decrypt/<key>/<token>')
+def decrypt_personnalise(key, token):
+    try:
+        fernet = Fernet(key.encode())
+        token_bytes = token.encode()
+        decrypted = fernet.decrypt(token_bytes)
+        return f"Valeur décryptée : {decrypted.decode()}"
+    except Exception as e:
+        return f"Erreur lors du déchiffrement : {str(e)}"
+
+
+from cryptography.fernet import Fernet
+print(Fernet.generate_key().decode())
+
+                                                                                                                       
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm
